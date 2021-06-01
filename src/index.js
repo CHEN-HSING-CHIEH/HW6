c738a630e2cd5391fd26ea31e71486095154b135
CHEN-HSING-CHIEH <alice2001090503@gmail.com>
2021-06-02 02:02:39 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import DatePickers from './date';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DatePickers />
   </React.StrictMode>,
   document.getElementById('root')
 );
